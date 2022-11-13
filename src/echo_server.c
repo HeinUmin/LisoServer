b22646e64051c6dbe0392784937e740577c4d81b
wyh2190980735 <wyh2190980735@yeah.net>
2022-11-13 06:07:24 +0000

    ✨ feat: Concurrent implement
    Complete echo_server with parallel.

--- src/echo_server.c
+++ src/echo_server.c
@@ -1,22 +1,11 @@
-/******************************************************************************
-* echo_server.c                                                               *
-*                                                                             *
-* Description: This file contains the C source code for an echo server.  The  *
-*              server runs on a hard-coded port and simply write back anything*
-*              sent to it by connected clients.  It does not support          *
-*              concurrent clients.                                            *
-*                                                                             *
-* Authors: Athula Balachandran <abalacha@cs.cmu.edu>,                         *
-*          Wolf Richter <wolf@cs.cmu.edu>                                     *
-*                                                                             *
-*******************************************************************************/
-
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include "parse.h"
 
@@ -33,16 +22,18 @@ int close_socket(int sock)
     return 0;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     int sock, client_sock;
     ssize_t readret;
     socklen_t cli_size;
     struct sockaddr_in addr, cli_addr;
     char buf[BUF_SIZE];
+    int max_fd, nready, i, nread;
+    fd_set rfds, rset;
 
     fprintf(stdout, "----- Echo Server -----\n");
-    
+
     /* all networked programs must create a socket */
     if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
     {
@@ -55,13 +46,12 @@ int main(int argc, char* argv[])
     addr.sin_addr.s_addr = INADDR_ANY;
 
     /* servers bind sockets to ports---notify the OS they accept connections */
-    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)))
+    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)))
     {
         close_socket(sock);
         fprintf(stderr, "Failed binding socket.\n");
         return EXIT_FAILURE;
     }
-
 
     if (listen(sock, 5))
     {
@@ -70,65 +60,103 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
+    FD_ZERO(&rfds);
+    FD_SET(sock, &rfds);
+    max_fd = sock;
+
     /* finally, loop waiting for input and then write it back */
     while (1)
     {
-        cli_size = sizeof(cli_addr);
-        if ((client_sock = accept(sock, (struct sockaddr *) &cli_addr,
-                                    &cli_size)) == -1)
+        rset = rfds;
+        nready = select(FD_SETSIZE, &rset, NULL, NULL, NULL);
+
+        if (nready < 1)
         {
-            close(sock);
-            fprintf(stderr, "Error accepting connection.\n");
+            fprintf(stderr, "Select error.\n");
             return EXIT_FAILURE;
         }
 
-        readret = 0;
-
-        while((readret = recv(client_sock, buf, BUF_SIZE, 0)) >= 1)
+        if (FD_ISSET(sock, &rset))
         {
-            Request *request = parse(buf,readret,client_sock);
-            if (request == NULL)
+            cli_size = sizeof(cli_addr);
+            if ((client_sock = accept(sock, (struct sockaddr *)&cli_addr,
+                                      &cli_size)) == -1)
             {
-                memset(buf, 0, BUF_SIZE);
-                strcpy(buf, "HTTP/1.1 400 Bad Request\r\n\r\n");
-                readret = strlen(buf);
-            }
-            else if (!strcmp(request->http_method, "GET") &&
-                !strcmp(request->http_method, "HEAD") &&
-                !strcmp(request->http_method, "POST"))
-            {
-                memset(buf, 0, BUF_SIZE);
-                strcpy(buf, "HTTP/1.1 501 Not Implemented\r\n\r\n");
-                readret = strlen(buf);
-            }
-            if (send(client_sock, buf, readret, 0) != readret)
-            {
-                close_socket(client_sock);
-                close_socket(sock);
-                fprintf(stderr, "Error sending to client.\n");
+                close(sock);
+                fprintf(stderr, "Error accepting connection.\n");
                 return EXIT_FAILURE;
             }
-            memset(buf, 0, BUF_SIZE);
-            if(request!=NULL)
-            {
-                free(request->headers);
-                free(request);
-            }
-        } 
+            FD_SET(client_sock, &rfds);
 
-        if (readret == -1)
-        {
-            close_socket(client_sock);
-            close_socket(sock);
-            fprintf(stderr, "Error reading from client socket.\n");
-            return EXIT_FAILURE;
+            if (client_sock > max_fd)
+                max_fd = client_sock;
+            if (--nready == 0)
+                continue;
         }
 
-        if (close_socket(client_sock))
+        for (i = sock + 1; i <= max_fd; i++)
         {
-            close_socket(sock);
-            fprintf(stderr, "Error closing client socket.\n");
-            return EXIT_FAILURE;
+            if (FD_ISSET(i, &rset))
+            {
+                readret = 0;
+                ioctl(i, FIONREAD, &nread);
+
+                if (nread == 0)
+                {
+                    if (close_socket(i))
+                    {
+                        fprintf(stderr, "Error closing client socket.\n");
+                        return EXIT_FAILURE;
+                    }
+                    else
+                    {
+                        FD_CLR(i, &rfds);
+                        printf("removing client on fd %d\n", i);
+                    }
+                }
+                else if ((readret = recv(i, buf, BUF_SIZE, 0)) >= 1)
+                {
+                    Request *request = parse(buf, readret, i);
+                    if (request == NULL)
+                    {
+                        memset(buf, 0, BUF_SIZE);
+                        strcpy(buf, "HTTP/1.1 400 Bad Request\r\n\r\n");
+                        readret = strlen(buf);
+                    }
+                    else if (strcmp(request->http_method, "GET") &&
+                             strcmp(request->http_method, "HEAD") &&
+                             strcmp(request->http_method, "POST"))
+                    {
+                        memset(buf, 0, BUF_SIZE);
+                        strcpy(buf, "HTTP/1.1 501 Not Implemented\r\n\r\n");
+                        readret = strlen(buf);
+                    }
+                    if (send(i, buf, readret, 0) != readret)
+                    {
+                        FD_CLR(i, &rfds);
+                        close_socket(i);
+                        fprintf(stderr, "Error sending to client.\n");
+                        return EXIT_FAILURE;
+                    }
+                    memset(buf, 0, BUF_SIZE);
+                    if (request != NULL)
+                    {
+                        free(request->headers);
+                        free(request);
+                    }
+                }
+
+                if (readret == -1)
+                {
+                    FD_CLR(i, &rfds);
+                    close_socket(i);
+                    fprintf(stderr, "Error reading from client socket.\n");
+                    return EXIT_FAILURE;
+                }
+
+                if (--nready == 0)
+                    break;
+            }
         }
     }
 
